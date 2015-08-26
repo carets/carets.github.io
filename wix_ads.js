0010fa2b388b5aed89f4a368431e163e73e2f276
Cooper Johnston <sverige.cooper@gmail.com>
2015-08-25 17:25:20 -0700

    added wix ad-hiding script

---
+++ wix_ads.js
@@ -0,0 +1,10 @@
+//Wix Ad Hiding Script
+//Ooh Cheaty
+
+function hideAds() {
+	var adBottom = document.getElementById("WIX_ADSdesktopWADBottom");
+	var adTop = document.getElementById("WIX_ADSdesktopWADTop");
+
+	adBottom.style.display = "none";
+	adTop.style.display = "none";
+}
